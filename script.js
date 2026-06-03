fb34faf7cab54e6e4c727c8f46ce55c800d24de1
Alicite <lunacarter1503@gmail.com>
2026-06-03 20:01:47 -0300

    aula 02/06

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const fetchGETAlunos = async () => {
     try {
-        let resposta = await fetch('http://localhost:3000/users', {
-            headers: {
-                "Access-Control-Allow-Origin": "http://localhost:3000/"
-            }
-        });
+        let resposta = await fetch('http://localhost:3000/users');
         resposta = await resposta.json();
         return resposta;
     } catch (e) {
@@ -13,40 +9,56 @@ const fetchGETAlunos = async () => {
 };
 
 const enviarAluno = async () => {
-    const nome = document.querySelector("#novo-nome");
-    const idade = document.querySelector("#novo-idade");
+    const nome = document.querySelector("#novo-nome").value;
+    const idade = document.querySelector("#novo-idade").value;
     const feedback = document.querySelector('#feedback');
 
     try {
         const options = {
             method: 'POST',
-            body: {
-                user: { nome, idade, idiomas: ["pt-BR"] }
-            },
             headers: {
-                "Access-Control-Allow-Origin": "http://localhost:3000/"
-            }
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({user: { nome, idade, idiomas: ["pt-BR"] }})
         }
         let resposta = await fetch('http://localhost:3000/users', options);
         resposta = await resposta.json();
-        feedback.innerHTML = resposta
-
+        feedback.textContent = resposta;
 
     } catch (e) {
         console.error(e);
     }
 }
 
+const removerAluno = async (event) => {
+    const id = event.target.dataset.id;
+    try {
+        await fetch(`http://localhost:3000/users/${id}`, {
+            method: 'DELETE'
+        });
+        event.target.parentElement.remove();
+    } catch (e) {
+        console.error(e);
+    }
+};
+
 const main = async () => {
     const alunos = await fetchGETAlunos();
     const elemUl = document.querySelector('#lista-alunos');
 
     for (let aluno of alunos) {
+        console.log(aluno);
         const elemLi = document.createElement('li');
 
         const idiomas = aluno.idiomas ? aluno.idiomas : 'Não foram encontrados idiomas';
 
         elemLi.innerHTML = `Nome: ${aluno.nome} - Idade: ${aluno.idade} - Idiomas: ${idiomas}`;
+        const btRemover = document.createElement('button');
+        btRemover.dataset.id = aluno._id;
+        btRemover.textContent = '❌';
+        btRemover.onclick = removerAluno;
+
+        elemLi.appendChild(btRemover);
         elemUl.appendChild(elemLi);
     }
 }
@@ -54,30 +66,32 @@ const main = async () => {
 const botaoEnviar = document.querySelector('#enviar');
 main()
 
-botaoEnviar.addEventListener('click', async () => {
-    const nome = document.querySelector("#novo-nome");
-    const idade = document.querySelector("#novo-idade");
-    const feedback = document.querySelector('#feedback');
+botaoEnviar.addEventListener('click', enviarAluno);
+// botaoEnviar.addEventListener('click', async () => {
+//     const nome = document.querySelector("#novo-nome").value;
+//     const idade = document.querySelector("#novo-idade").value;
+//     const feedback = document.querySelector('#feedback');
 
-    const body = { user: { nome, idade } };
+//     const body = { user: { nome, idade } };
+    
+//     try {
+//         const options = {
+//             method: 'POST',
+//             body: Object.keys(body)
+//             .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(body[k])}`)
+//             .join('&'),
+//             headers: {
+//                 "Access-Control-Allow-Origin": "http://localhost:3000/"
+//             }
+//         }
+//         console.log(options);
+//         let resposta = await fetch('http://localhost:3000/users', options);
+//         resposta = await resposta.json();
+//         feedback.innerHTML = resposta;
+//         console.log(resposta);
 
-    try {
-        const options = {
-            method: 'POST',
-            body: Object.keys(body)
-                .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(body[k])}`)
-                .join('&'),
-            headers: {
-                "Access-Control-Allow-Origin": "http://localhost:3000/"
-            }
-        }
-        let resposta = await fetch('http://localhost:3000/users', options);
-        resposta = await resposta.json();
-        feedback.innerHTML = resposta;
-        console.log(resposta);
 
-
-    } catch (e) {
-        console.error(e);
-    }
-});
+//     } catch (e) {
+//         console.error(e);
+//     }
+// });
